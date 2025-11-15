d36fb4dd74a1d02806d06313d2005ea1c5d4530f
sunisaDzt <sunisa.dzentric@gmail.com>
2025-11-15 10:50:16 +0700

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Sarabun } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const sarabun = Sarabun({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
